4e9a083f35226766363ffc4ce21f0c7abce3dc26
Opilane TTHK <opilane@tthk.ee>
2020-11-27 10:02:20 +0200

    first commit

---
+++ Pet/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Pet
+{
+    class Program
+    {
+        class pet
+        {
+            string name;
+            int age;
+            double weight;
+            int lifespan;
+            public Pet(string _name, double _weight)
+            {
+                name = _name;
+                weight = _weight;
+            }
+            public int Lifespan
+            {
+                get { return lifespan; }
+            }
+            public double Age
+            {
+                get { return age; }
+            }
+            public void Eat()
+            {
+                Console.WriteLine("Eating routine");
+                Console.WriteLine("Weight before: " + weight);
+                weight += 0.2;
+                Console.WriteLine("Weight after: " + weight);
+            }
+            public void Exercise()
+            {
+                Console.WriteLine("Exercise routine");
+                Console.WriteLine("Weight before: " + weight);
+                weight -= 0.1;
+                Console.WriteLine("Weight after: " );
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
