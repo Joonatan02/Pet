1330b00e74566ffeaff241d978e9e1e32bb40fab
Joonatan02 <70571163+Joonatan02@users.noreply.github.com>
2020-11-30 17:28:27 +0200

    Update Program.cs

--- Pet/Program.cs
+++ Pet/Program.cs
@@ -4,44 +4,71 @@ namespace Pet
 {
     class Program
     {
-        class pet
+        class Pet
+    {
+        string name;
+        int age;
+        double weight;
+        int lifespan;
+        
+        public Pet(string _name, double _weight)
         {
-            string name;
-            int age;
-            double weight;
-            int lifespan;
-            public Pet(string _name, double _weight)
-            {
-                name = _name;
-                weight = _weight;
-            }
-            public int Lifespan
-            {
-                get { return lifespan; }
-            }
-            public double Age
-            {
-                get { return age; }
-            }
-            public void Eat()
-            {
-                Console.WriteLine("Eating routine");
-                Console.WriteLine("Weight before: " + weight);
-                weight += 0.2;
-                Console.WriteLine("Weight after: " + weight);
-            }
-            public void Exercise()
-            {
-                Console.WriteLine("Exercise routine");
-                Console.WriteLine("Weight before: " + weight);
-                weight -= 0.1;
-                Console.WriteLine("Weight after: " );
-
-            }
+            name = _name;
+            weight = _weight;
         }
+        public int Lifespan
+        {
+            get { return lifespan; }
+        }
+        public double Weight
+        {
+            get { return weight; }
+        }
+        public double Age
+        {
+            get { return age; }
+        }
+        public void Eat()
+        {
+            Console.WriteLine("Eat routine");
+            Console.WriteLine("Weight before: " + weight);
+            weight += 0.2;
+            Console.WriteLine("Weight after: " +weight);
+        }
+        public void Exercise()
+        {
+            Console.WriteLine("Exercise routine");
+            Console.WriteLine("Weight before: " + weight);
+            weight -= 0.1;
+            Console.WriteLine("Weight after: " + weight);
+        }
+        public void Display()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Weight: " + weight);
+            Console.WriteLine("Lifespan: " + lifespan);
+        }
+
+    }
+    class Program
+    {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Pet newHamster = new Pet("Luud", 0.01);
+
+            for (int age = 0; age < 10; age++)
+            {
+                if (newHamster.Weight > 0.7)
+                {
+                    newHamster.Exercise();
+                }
+               else if (newHamster.Weight < 0.7)
+                {
+                    newHamster.Eat();
+                }
+                Console.WriteLine("Iteration: " + age);
+            }
         }
     }
 }
